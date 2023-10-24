62c7283f2a2a16b9edce178de069bc4ff382d0eb
enemuo <francenemuo@gmail.com>
2023-10-24 11:24:07 +0100

    feat: add fruit cards

---
+++ src/app/components/Products.tsx
@@ -0,0 +1,39 @@
+const fruits = [
+    {
+      id: 1,
+      name: "Berries",
+      description: "Berries are small, succulent fruits with thin skins that burst with flavor. Highly perishable, they require careful handling to maintain their freshness. Blackberries, cranberries, blueberries, red and black raspberries, strawberries, and grapes are common examples of berries, each offering a unique combination of sweetness and tartness. These fruits are versatile, perfect for snacking, making jams, or enhancing various culinary creations with their vibrant colors and refreshing taste.",
+      examples: ["blackberries", "cranberries", "blueberries", "red and black raspberries", "strawberries", "grapes"]
+    },
+    {
+      id: 2,
+      name: "Pits",
+      description: "Pitted fruits are defined by their soft, fleshy exterior encasing a single, hard stone or pit containing the seed. Cherries, apricots, nectarines, peaches, and plums are prime examples of these delectable fruits. The pits can be both a challenge and a reward, as they house the seed that can be either a nuisance or a treat for those who enjoy the distinctive flavor and texture of these fruits. Pitted fruits are often used in baking, preserves, or enjoyed fresh, providing a delightful mix of sweetness and juiciness.",
+      examples: ["cherries", "apricots", "nectarines", "peaches", "plums"]
+    },
+    {
+      id: 3,
+      name: "Core",
+      description: "Core fruits are characterized by their central, seed-containing core surrounded by a thick layer of flesh. Apples and pears are classic representatives of this category, known for their crisp texture and slightly sweet taste. The core, which contains the seeds, is typically removed before consumption. These fruits are commonly used in a wide range of culinary applications, from pies and crisps to salads, and they offer a delightful balance of sweetness and a slight hint of tartness.",
+      examples: ["apples", "pears"]
+    },
+    {
+      id: 4,
+      name: "Citrus Fruits",
+      description: "Citrus fruits are distinguished by their thick outer rind and segmented flesh separated by thin membranes. Oranges, tangerines, grapefruits, kumquats, lemons, and limes are prominent members of this zesty category. Their bright, tangy flavors are a hallmark of these fruits, making them a popular choice for juices, garnishes, and culinary seasoning. The thick rind contains essential oils that contribute to their distinct aroma and flavor, while the segmented flesh is a juicy, refreshing delight.",
+      examples: ["oranges", "tangerines", "grapefruits", "kumquats", "lemons", "limes"]
+    },
+    {
+      id: 5,
+      name: "Melons",
+      description: "Melons are large, juicy fruits with thick skins and numerous seeds. Varieties such as cantaloupe, casaba, honeydew, and watermelon offer a delightful sweetness and a refreshing, hydrating quality. Their thick, protective skins keep the fruit inside succulent and flavorful. Melons are perfect for summer treats, salads, and smoothies, making them a popular choice for quenching thirst and satisfying the sweet tooth simultaneously.",
+      examples: ["cantaloupe", "casaba", "honeydew", "watermelon"]
+    },
+    {
+      id: 6,
+      name: "Tropical Fruits",
+      description: "Tropical fruits, cultivated in warm climates and often considered exotic, bring a taste of the tropics to various parts of the world. Avocados, coconuts, bananas, figs, dates, guavas, mangoes, papayas, pineapples, pomegranates, and kiwi are prime examples of these exotic delights. Their flavors can range from creamy and rich to sweet and tangy, offering a wide spectrum of taste experiences. These fruits are often enjoyed fresh, in smoothies, desserts, and savory dishes, bringing a taste of paradise to the table.",
+      examples: ["avocados", "coconut", "bananas", "figs", "dates", "guavas", "mangoes", "papayas", "pineapples", "pomegranates", "kiwi"]
+    }
+  ];
+  
